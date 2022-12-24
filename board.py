da32cfa18fc1f858f2bad2068a5d008071d9231c
DABER364 <david16bernier@hotmail.ca>
2022-12-24 00:44:42 -0500

    modif

--- board.py
+++ board.py
@@ -3,7 +3,12 @@ class Board():
         self.board = self.makeBoard()
 
     def __str__(self) -> str:
-        return self.board
+        string = ''
+        for y in self.board:
+            for x in y:
+                string += '| '+ x +' |'
+            string += '\n------------\n'
+        return string
 
     def makeBoard(self):
         plateau = []
@@ -11,3 +16,9 @@ class Board():
         for x in range(3):
             plateau.append(ligne)
         return plateau
+
+    def placer(self, joueur, position):
+        if joueur is 1:
+            self.board[2][0] = 'X'
+        if joueur is 2:
+            self.board[position[1]][position[0]] = 'O'
--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from board import Board
 if __name__ == "__main__":
     board = Board()
     print(board)
+    board.placer(1, [0, 0])
+    print(board)
+    board.placer(2, [1, 0])
+    print(board)
