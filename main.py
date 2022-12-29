05fd6400b7887ce53437d3e752066fbc5a3ef274
DABER364 <david16bernier@hotmail.ca>
2022-12-28 21:57:22 -0500

    simplification du main
    il est maintenant jouable

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from board import Board
 
 def recup_move(joueur):
     #manque verif
+    print('Tour Joueur',joueur,':')
     pos = input("Donnez la position où appliquer ce coup (x,y):")
     return (joueur, [int(i) for i in pos.split(',')])
 
 if __name__ == "__main__":
     board = Board()
     while board.est_terminee() is False:
-        print(board)
-        print('Tour Joueur 1:')
-        joueur, position = recup_move(1)
-        board.placer(joueur, position)
-        print(board)
-        print('Tour Joueur 2:')
-        joueur, position = recup_move(2)
-        board.placer(joueur, position)
+        for x in [1, 2]:
+            print(board)
+            joueur, position = recup_move(x)
+            board.placer(joueur, position)
+            if board.est_terminee():
+                print(board)
+                break
+            
     
