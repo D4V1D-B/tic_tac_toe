f47e5b28ce9493515f988e1823387f64a346cef4
DABER364 <david16bernier@hotmail.ca>
2022-12-28 21:45:20 -0500

    modif main et board
    le jeu est semi fonctionnel

--- board.py
+++ board.py
@@ -3,22 +3,40 @@ class Board():
         self.board = self.makeBoard()
 
     def __str__(self) -> str:
-        string = ''
-        for y in self.board:
-            for x in y:
-                string += '| '+ x +' |'
-            string += '\n------------\n'
-        return string
+        string = (f'{self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]}'+
+        f'\n---------'+
+        f'\n{self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]}'+
+        f'\n---------'+
+        f'\n{self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]}')
+        return string + '\n'
 
     def makeBoard(self):
         plateau = []
-        ligne = [' ', ' ', ' ']
-        for x in range(3):
-            plateau.append(ligne)
+        for _ in range(3):
+            plateau.append([' ', ' ', ' '])
         return plateau
 
     def placer(self, joueur, position):
+        #manque verif
         if joueur is 1:
-            self.board[2][0] = 'X'
+            self.board[position[1]][position[0]] = 'X'
         if joueur is 2:
             self.board[position[1]][position[0]] = 'O'
+
+    def est_terminee(self):
+        #ligne et colonne
+        for joue in ['O', 'X']:
+            for x in range(0,2):
+                #ligne
+                if self.board[x][0] is joue and self.board[x][1] is joue and self.board[x][2] is joue:
+                    return True
+                #colonne
+                if self.board[0][x] is joue and self.board[1][x] is joue and self.board[2][x] is joue:
+                    return True
+            #diago 1
+            if self.board[0][0] is joue and self.board[1][1] is joue and self.board[2][2] is joue:
+                return True
+            #diago 2
+            if self.board[2][0] is joue and self.board[1][1] is joue and self.board[0][2] is joue:
+                return True
+        return False
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from board import Board
 
+
+def recup_move(joueur):
+    #manque verif
+    pos = input("Donnez la position où appliquer ce coup (x,y):")
+    return (joueur, [int(i) for i in pos.split(',')])
+
 if __name__ == "__main__":
     board = Board()
-    print(board)
-    board.placer(1, [0, 0])
-    print(board)
-    board.placer(2, [1, 0])
-    print(board)
+    while board.est_terminee() is False:
+        print(board)
+        print('Tour Joueur 1:')
+        joueur, position = recup_move(1)
+        board.placer(joueur, position)
+        print(board)
+        print('Tour Joueur 2:')
+        joueur, position = recup_move(2)
+        board.placer(joueur, position)
+    
